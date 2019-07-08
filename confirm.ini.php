ed22511a93699170d6af5e4adf0b2c7475fe6750
obentnet <obent@qq.com>
2019-07-09 01:20:43 +0800

    配置信息文件

---
+++ confirm.ini.php
@@ -0,0 +1,43 @@
+
+    <?php
+    /**
+     * 这是个属于系统配置的文件，如果不懂请不要随意更改~
+    */
+    ?>
+    
+    <?php
+
+
+    /** 基本设置 */
+    $SiteUrl = 'localhost';
+    $UserName = 'admin';
+    $Password = '123456';
+    $SiteName = 'YuJie';
+    $SiteHead = 'https://yujienb.cn/head.jpg';
+    $SiteSay  = '666';
+
+    $DefaultTheme = 'themes/default';
+
+    /**链接设置 */
+
+    $QQ_Link = 'QQLINK';
+    $WeChat_Link = 'WeiChatlink';
+    $Sina_Link = 'SinaLinks';
+    $Netease_Link = 'NetEaseLink';
+    $Mail_Link = 'MailLink';
+    $GitHub_Link = 'GithubLink';
+    $FaceBook_Link = 'FaceBookLink';
+    $Steam_Link = 'SteamLink';
+
+    /**系统内设*/
+    
+    $ThemeName = 'default';
+
+    $ThemeURL = '/themes/'.$ThemeName;
+
+    $MDUI_CSS = 'https://cdnjs.loli.net/ajax/libs/mdui/0.4.2/css/mdui.min.css';
+
+    $MDUI_JS = 'https://cdnjs.loli.net/ajax/libs/mdui/0.4.2/js/mdui.min.js';
+
+    ?>
+    
